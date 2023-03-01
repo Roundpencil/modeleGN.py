32e61a91fcab16307060b7e700733068042fd5ef
work <pi-work@ssji.net>
2023-03-01 11:34:45 +0100

    intégration credential dans code

--- lecteurGoogle.py
+++ lecteurGoogle.py
@@ -3,10 +3,11 @@ from __future__ import print_function
 import os.path
 
 from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
+
+import credentials
 
 # If modifying these scopes, delete the file token.json.
 # SCOPES = ['https://www.googleapis.com/auth/drive.readonly https://www.googleapis.com/auth/documents.readonly
@@ -29,15 +30,22 @@ def creer_lecteurs_google_apis():
     # The file token.json stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
     # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
+
+    # ServiceAccountCredentials.from_json_keyfile_dict(credentials.app_creds_dic, SCOPES)
+
+    # if os.path.exists('token.json'):
+    #     creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # # If there are no (valid) credentials available, let the user log in.
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
+            flow = InstalledAppFlow.from_client_config(
+                credentials.app_creds_dic,
+                SCOPES)
+
+            # flow = InstalledAppFlow.from_client_secrets_file(
+            #     'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
