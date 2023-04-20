c6730badff22004e29f98be0b0b6a4b78ff61c5c
work <pi-work@ssji.net>
2023-04-20 10:36:46 +0200

    construction step 2 GUI en cours

--- IHM_step_two.py
+++ IHM_step_two.py
@@ -1,3 +1,4 @@
+import configparser
 from enum import Enum
 from tkinter import ttk
 import tkinter as tk
@@ -43,6 +44,8 @@ class NotebookFrame(ttk.Frame):
             self.mes_panneaux[v_parametre] = panneau
             self.notebook.add(panneau, text=nom_tab)
 
+        self.mes_panneaux[self.ParamsMultiples.INTRIGUES.value].set_entrees_min(1)
+
     def get_all_parametres(self):
         to_return = []
         for panneau in self.mes_panneaux:
@@ -55,7 +58,7 @@ class NotebookFrame(ttk.Frame):
         tuples_intrigues = self.mes_panneaux[self.ParamsMultiples.INTRIGUES.value].get_tuples_parametres()
         # print(f"tuples = {tuples_intrigues}")
         dict_essentiel = dict_essentiel | dict(tuples_intrigues)
-        print(f"dict_essentiel : {dict_essentiel}")
+        # print(f"dict_essentiel : {dict_essentiel}")
 
         tuples_pjs = self.mes_panneaux[self.ParamsMultiples.PJS.value].get_tuples_parametres()
         tuples_pnjs = self.mes_panneaux[self.ParamsMultiples.PNJS.value].get_tuples_parametres()
@@ -67,10 +70,23 @@ class NotebookFrame(ttk.Frame):
                          dict(tuples_pnjs) | \
                          dict(tuples_evenements) | \
                          dict(tuples_objets)
-        print(f"dict_optionnel : {dict_optionnel}")
+        # print(f"dict_optionnel : {dict_optionnel}")
 
-        dict_essentiel = self.retirer_clefs_vides(dict_essentiel)
-        dict_optionnel = self.retirer_clefs_vides(dict_optionnel)
+        # dict_essentiel = self.retirer_clefs_vides(dict_essentiel)
+        # dict_optionnel = self.retirer_clefs_vides(dict_optionnel)
+
+        # fabrication d'un config parser
+        config = configparser.ConfigParser()
+        config.add_section('Essentiels')
+        for param in dict_essentiel:
+            config.set("Essentiels", param, dict_essentiel[param])
+
+        config.add_section('Optionnels')
+        for param in dict_optionnel:
+            config.set("Optionnels", param, dict_optionnel[param])
+
+        with open('config_text.ini', 'w') as configfile:
+            config.write(configfile)
 
     def retirer_clefs_vides(self, d:dict):
         return {key: value for key, value in d.items() if value != ''}
@@ -78,7 +94,8 @@ class NotebookFrame(ttk.Frame):
 class PremierPanneau(ttk.Frame):
     def __init__(self, parent=None, *args, **kwargs):
         super().__init__(parent, *args, **kwargs)
-        self.mes_widgets = set()
+        self.mes_widgets_essentiels = {}
+        self.mes_widgets_optionnels = {}
         self.essentials_frame = None
         self.optionals_frame = None
 
@@ -114,6 +131,9 @@ class PremierPanneau(ttk.Frame):
 
             entry = ttk.Entry(self.essentials_frame, name=f'{key}_entry')
             entry.grid(column=1, row=index, padx=10, pady=5, sticky="ew")
+            # mon_widget = WidgetPremierPanneau(self.essentials_frame, label_text, key, False)
+            # mon_widget.grid(column=0, row=index, padx=10, pady=5, sticky="w")
+            # self.mes_widgets_essentiels[key] = mon_widget
 
         self.essentials_frame.columnconfigure(1, weight=1)
 
@@ -122,13 +142,36 @@ class PremierPanneau(ttk.Frame):
         self.optionals_frame.pack(pady=10, padx=10, fill="x")
 
         for index, (key, label_text) in enumerate(self.optional_params.items()):
-            label = ttk.Label(self.optionals_frame, text=label_text)
-            label.grid(column=0, row=index, padx=10, pady=5, sticky="w")
-
+            #     label = ttk.Label(self.optionals_frame, text=label_text)
+            #     label.grid(column=0, row=index, padx=10, pady=5, sticky="w")
+            #
+            #     entry = ttk.Entry(self.optionals_frame, name=f'{key}_entry')
+            #     entry.grid(column=1, row=index, padx=10, pady=5, sticky="ew")
+            #
+            # self.optionals_frame.columnconfigure(1, weight=1)
+            var = tk.BooleanVar(value=True)
             entry = ttk.Entry(self.optionals_frame, name=f'{key}_entry')
-            entry.grid(column=1, row=index, padx=10, pady=5, sticky="ew")
 
-        self.optionals_frame.columnconfigure(1, weight=1)
+            chk = ttk.Checkbutton(self.optionals_frame, variable=var,
+                                  command=lambda e=self.optionals_frame.nametowidget(f'{key}_entry'),
+                                                 v=var: self.toggle_entry(e, v))
+            chk.grid(column=0, row=index, padx=(10, 0), pady=5, sticky="w")
+
+            label = ttk.Label(self.optionals_frame, text=label_text)
+            label.grid(column=1, row=index, padx=(0, 10), pady=5, sticky="w")
+
+            entry.grid(column=2, row=index, padx=10, pady=5, sticky="ew")
+
+            chk.invoke()  # Set the initial state of the checkboxes and entries
+
+            self.optionals_frame.columnconfigure(2, weight=1)
+
+    def toggle_entry(self, entry, var):
+        if var.get():
+            entry['state'] = 'normal'
+        else:
+            # entry.delete(0, tk.END)  # Clear the entry
+            entry['state'] = 'disabled'
 
     def generer_dictionnaires_parametres(self):
         # tuples = []
@@ -147,20 +190,65 @@ class PremierPanneau(ttk.Frame):
         #                    for key in self.essential_params}
 
         for key in self.optional_params:
+            # entry = self.optionals_frame.nametowidget(f'{key}_entry')
+            # #     tuples.append((key, entry.get()))
+            # dict_optionnels[key] = entry.get()
             entry = self.optionals_frame.nametowidget(f'{key}_entry')
-            #     tuples.append((key, entry.get()))
-            dict_optionnels[key] = entry.get()
-
+            print(f"debug : {entry}, {entry['state']}, {entry.get()}")
+            if entry['state'] == 'normal':
+                dict_optionnels[key] = entry.get()
+            # else:
+            #     dict_optionnels[key] = ''
         # print(tuples)
         return dict_essentiels, dict_optionnels
 
 
+# class WidgetPremierPanneau(ttk.Frame):
+#     def __init__(self, parent, label_text, nom_parametre, avec_box=False):
+#         super().__init__(parent)
+#
+#         self.nom_parametre = nom_parametre
+#         self.var = tk.BooleanVar(value=True)
+#
+#         if avec_box:
+#             self.chk = ttk.Checkbutton(self, variable=self.var, command=self.toggle_entry)
+#             self.chk.grid(column=0, row=0, padx=(10, 0), pady=5, sticky="w")
+#             self.chk.invoke()  # Set the initial state of the checkbox and entry
+#
+#         self.label = ttk.Label(self, text=label_text)
+#         self.label.grid(column=1, row=0, padx=(0, 10), pady=5, sticky="w")
+#
+#         self.entry = ttk.Entry(self)
+#         self.entry.grid(column=2, row=0, padx=10, pady=5, sticky="ew")
+#
+#         self.columnconfigure(2, weight=1)
+#
+#
+#
+#     def toggle_entry(self):
+#         if self.var.get():
+#             self.entry['state'] = 'normal'
+#         else:
+#             self.entry.delete(0, tk.END)  # Clear the entry
+#             self.entry['state'] = 'disabled'
+#
+#     def get_tuple_champ_entree(self):
+#         if self.var.get():
+#             return self.nom_parametre, self.entry.get()
+#         else:
+#             return None, None
+
+
 class PanneauParametresMultiples(ttk.Frame):
-    def __init__(self, parent, prefixe_parametre):
+    def __init__(self, parent, prefixe_parametre, entrees_min = 0):
         super().__init__(parent)
+
 
         self.mes_widgets = set()
         self.prefixe_parametre = prefixe_parametre
+
+        self.entree_min = entrees_min
+        self.set_entrees_min(entrees_min)
 
         # self.row = 0
         # self.title("Editeur de fichier de configuration")
@@ -186,7 +274,7 @@ class PanneauParametresMultiples(ttk.Frame):
     def get_tuples_parametres(self):
         return [widget.get_tuple_champ_entree() for widget in self.mes_widgets]
 
-    def add_button_click(self):
+    def add_widget_entree(self):
         widget_a_rajouter = widget_entree(self, self.retirer_widget_entree,
                                           prefixe_parametre=self.prefixe_parametre
                                           )
@@ -194,15 +282,30 @@ class PanneauParametresMultiples(ttk.Frame):
         widget_a_rajouter.grid(row=no_ligne, column=0, columnspan=4)
         self.mes_widgets.add(widget_a_rajouter)
 
-        print("Add button clicked")
-        print(f"{[widget.get_tuple_champ_entree() for widget in self.mes_widgets]}")
+    def set_entrees_min(self, entree_min: int):
+        self.entree_min = entree_min
+        while len(self.mes_widgets) < self.entree_min:
+            self.add_widget_entree()
+
+    def add_button_click(self):
+        self.add_widget_entree()
+        # widget_a_rajouter = widget_entree(self, self.retirer_widget_entree,
+        #                                   prefixe_parametre=self.prefixe_parametre
+        #                                   )
+        # no_ligne = len(self.mes_widgets) + 1
+        # widget_a_rajouter.grid(row=no_ligne, column=0, columnspan=4)
+        # self.mes_widgets.add(widget_a_rajouter)
+
+        # print("Add button clicked")
+        # print(f"{[widget.get_tuple_champ_entree() for widget in self.mes_widgets]}")
 
     def retirer_widget_entree(self, widget_a_retirer: tk.Frame):
-        self.mes_widgets.remove(widget_a_retirer)
-        widget_a_retirer.destroy()
+        if len(self.mes_widgets) > self.entree_min:
+            self.mes_widgets.remove(widget_a_retirer)
+            widget_a_retirer.destroy()
 
 
-class widget_entree(tk.Frame):
+class widget_entree(ttk.Frame):
     def __init__(self, master, fonction_destruction, prefixe_parametre: str, nom_param: str = "",
                  valeur_param: str = ""):
         super().__init__(master=master)
