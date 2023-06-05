6f6a7e5dfa72243f1f43c3d20efa9b3d72edb715
work <pi-work@ssji.net>
2023-06-05 21:55:58 +0200

    début de l'ajout des fonction d'optimisation des évènements

--- zz_futur__tables_internvetions_PNJS.py
+++ zz_futur__tables_internvetions_PNJS.py
@@ -39,12 +39,18 @@ def creer_dict_intervenants_id(evenements: list[Evenement]):
 
 
 # préparer les données à partir des évènements
-def evenements_2_dict_ortools(gn: GN):
-    pas = determiner_pas(gn.evenements.values())
+def preparer_donnees_pour_ortools(gn: GN, pas=None):
+    if pas is None:
+        pas = determiner_pas(gn.evenements.values())
     dict_intervenants_id = creer_dict_intervenants_id(gn.evenements.values())
 
+    evenements_formattes = evenements_2_dict_ortools(dict_intervenants_id, gn.evenements.values(), pas)
+    return evenements_formattes, dict_intervenants_id
+
+
+def evenements_2_dict_ortools(dict_intervenants_id, liste_evenements: list[Evenement], pas):
     evenements_formattes = []
-    for evenement in gn.evenements.values():
+    for evenement in liste_evenements:
         for i, intervention in enumerate(evenement.interventions, start=1):
             # {"start": 0, "end": 4, "pnjs": [0, 1]},
             heure_debut = heure_en_pas(intervention.heure_debut, pas)
@@ -60,7 +66,7 @@ def evenements_2_dict_ortools(gn: GN):
                             "nom": f"{evenement.nom_evenement} - {i}"
                             }
             evenements_formattes.append(current_dict)
-    return evenements_formattes, dict_intervenants_id
+    return evenements_formattes
 
 
 def proposer_affectation_pnjs(nb_pnj, nb_personnes, dict_evenements_formatte, affectations_predefinies=None):
@@ -187,139 +193,7 @@ def afficher_resultats_solver(solver, status, nb_personnes, temps_total_par_pers
 
 ####################################################################################################################
 
-# # Définition des variables et des données d'exemple
-# nb_pnj = 6
-# nb_personnes = 4
-# evenements = [
-#     {"start": 0, "end": 4, "pnjs": [0, 1]},
-#     {"start": 2, "end": 6, "pnjs": [2, 3]},
-#     {"start": 5, "end": 9, "pnjs": [0]},
-#     {"start": 6, "end": 7, "pnjs": [4]},
-#     {"start": 15, "end": 16, "pnjs": [5]},
-#     {"start": 8, "end": 13, "pnjs": [1, 3, 2]},
-# ]
-#
-# affectations_predefinies = {
-#     1: 0,  # La personne 0 jouera le PNJ 1
-#     3: 2,  # La personne 2 jouera le PNJ 3
-# }
-#
-# # Création du modèle
-# model = cp_model.CpModel()
-#
-# # Création des variables pour le modèle
-# interventions = {}
-# for evt_id, evt in enumerate(evenements):
-#     for pnj in evt["pnjs"]:
-#         for p in range(nb_personnes):
-#             variable_name = f"evt_{evt_id}_pnj_{pnj}_p_{p}"
-#             interventions[variable_name] = model.NewBoolVar(variable_name)
-#
-# # Ajout des contraintes au modèle
-#
-# # Chaque PNJ doit être joué par une personne à la fois
-# for evt_id, evt in enumerate(evenements):
-#     for pnj in evt["pnjs"]:
-#         model.Add(sum(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] for p in range(nb_personnes)) == 1)
-#
-# # Les personnes ne peuvent pas jouer plusieurs PNJ en même temps
-# for p in range(nb_personnes):
-#     for t in range(max(evt["end"] for evt in evenements)):
-#         variables_a_temps_t = []
-#         for evt_id, evt in enumerate(evenements):
-#             for pnj in evt["pnjs"]:
-#                 if evt["start"] <= t < evt["end"]:
-#                     variables_a_temps_t.append(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"])
-#         if variables_a_temps_t:
-#             model.Add(sum(variables_a_temps_t) <= 1)
-#
-# # Un PNJ doit être joué par la même personne tout au long du jeu
-# for pnj in range(nb_pnj):
-#     for p1 in range(nb_personnes):
-#         for p2 in range(p1 + 1, nb_personnes):
-#             for evt_id1, evt1 in enumerate(evenements):
-#                 for evt_id2, evt2 in enumerate(evenements):
-#                     if pnj in evt1["pnjs"] and pnj in evt2["pnjs"]:
-#                         model.Add(interventions[f"evt_{evt_id1}_pnj_{pnj}_p_{p1}"] + interventions[
-#                             f"evt_{evt_id2}_pnj_{pnj}_p_{p2}"] <= 1)
-#
-# # Affectations prédéfinies
-# for pnj, personne in affectations_predefinies.items():
-#     for evt_id, evt in enumerate(evenements):
-#         if pnj in evt["pnjs"]:
-#             model.Add(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{personne}"] == 1)
-#
-# # Définition de l'objectif du modèle
-# # ancien objectif
-# # temps_total_par_personne = [model.NewIntVar(0, 100, f"temps_total_p_{p}") for p in range(nb_personnes)]
-# # for p in range(nb_personnes):
-# #     model.Add(temps_total_par_personne[p] == sum(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] * (evt["end"] - evt["start"]) for evt_id, evt in enumerate(evenements) for pnj in evt["pnjs"]))
-# # model.Minimize(sum(temps_total_par_personne))
-#
-# temps_total_par_personne = [model.NewIntVar(0, 100, f"temps_total_p_{p}") for p in range(nb_personnes)]
-# for p in range(nb_personnes):
-#     model.Add(temps_total_par_personne[p] == sum(
-#         interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] * (evt["end"] - evt["start"]) for evt_id, evt in
-#         enumerate(evenements) for pnj in evt["pnjs"]))
-#
-# plus_petit_temps = model.NewIntVar(0, 100, "plus_petit_temps")
-# for p in range(nb_personnes):
-#     model.Add(plus_petit_temps <= temps_total_par_personne[p])
-# model.Maximize(plus_petit_temps)
-#
-# # Résoudre le modèle
-# solver = cp_model.CpSolver()
-# status = solver.Solve(model)
-#
-# # état de la solution
-# print(f"solution = {status}")
 
-######################################################################################################################
-
-
-# # Afficher les résultats
-# if status in [cp_model.FEASIBLE, cp_model.OPTIMAL]:
-#     # if status == cp_model.OPTIMAL:
-#     print("Temps total par personne :")
-#     for p in range(nb_personnes):
-#         print(f"Personne {p}: {solver.Value(temps_total_par_personne[p])} minutes")
-#
-#     print("\nPlanning des interventions :")
-#     for evt_id, evt in enumerate(evenements):
-#         for pnj in evt["pnjs"]:
-#             for p in range(nb_personnes):
-#                 if solver.Value(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"]) == 1:
-#                     print(f"Personne {p} joue le PNJ {pnj} lors de l'événement {evt_id}")
-#                 else:
-#                     print("Pas de solution trouvée.")
-#
-# # Trouver la dernière heure de fin des événements
-# derniere_heure = max(evt["end"] for evt in evenements)
-#
-# # Afficher l'en-tête du planning
-# print("Heure", end=";")
-# for p in range(nb_personnes):
-#     print(f"Personne {p}", end=";")
-# print()
-#
-# # Afficher les lignes du planning pour chaque heure
-# for t in range(derniere_heure):
-#     print(t, end=";")
-#     for p in range(nb_personnes):
-#         pnj_et_evt = None
-#         for evt_id, evt in enumerate(evenements):
-#             for pnj in evt["pnjs"]:
-#                 if evt["start"] <= t < evt["end"] and solver.Value(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"]) == 1:
-#                     pnj_et_evt = f"PNJ {pnj} (Evt {evt_id})"
-#                     break
-#             if pnj_et_evt:
-#                 break
-#
-#         if pnj_et_evt:
-#             print(f"\t{pnj_et_evt}", end=";")
-#         else:
-#             print("-", end=";")
-#     print()
 
 
 def est_ok(x):
---
+++ zzz_adaptation_solver_en_dur.py
@@ -0,0 +1,164 @@
+from ortools.sat.python import cp_model
+## Code création données de test :
+#
+# from zz_futur__tables_internvetions_PNJS import *
+# from MAGnet import *
+# gn = GN.load('GN Buffy')
+# evenements = list(gn.evenements.values())
+# little_events = evenements[:5]
+# dict_evt_id = creer_dict_intervenants_id(little_events)
+# data_bruts = evenements_2_dict_ortools(dict_evt_id, little_events, 60)
+# data_bruts
+# # Définition des variables et des données d'exemple
+# from ortools.sat.python import cp_model
+# from modeleGN import *
+
+nb_pnj = 7
+nb_personnes = 4
+# evenements = [
+#     {"start": 0, "end": 4, "pnjs": [0, 1]},
+#     {"start": 2, "end": 6, "pnjs": [2, 3]},
+#     {"start": 5, "end": 9, "pnjs": [0]},
+#     {"start": 6, "end": 7, "pnjs": [4]},
+#     {"start": 15, "end": 16, "pnjs": [5]},
+#     {"start": 8, "end": 13, "pnjs": [1, 3, 2]},
+# ]
+
+evenements = [
+    {'start': 15, 'end': 16, 'pnjs': [2], 'nom': 'E027 - Fête des sports - 1'},
+    {'start': 12, 'end': 13, 'pnjs': [3], 'nom': 'E004-1 - coup de fil de mr wang - 1'},
+    {'start': 15, 'end': 16, 'pnjs': [5], 'nom': 'E029 - Le conseil des élèves - 1'},
+    {'start': 0, 'end': 1, 'pnjs': [4, 6], 'nom': 'E015-1- Il faut sauver Charlie - 1'},
+    {'start': 8, 'end': 9, 'pnjs': [1, 7], 'nom': 'E008-1 - appel du conseil des observateurs - 1'}]
+
+# evenements = [
+#     {'start': 15, 'end': 16, 'pnjs': [2]},
+#     {'start': 12, 'end': 13, 'pnjs': [3]},
+#     {'start': 15, 'end': 16, 'pnjs': [5]},
+#     {'start': 0, 'end': 1, 'pnjs': [4, 6]},
+#     {'start': 8, 'end': 9, 'pnjs': [1, 7]}]
+
+
+
+
+affectations_predefinies = {
+    1: 0,  # La personne 0 jouera le PNJ 1
+    3: 2,  # La personne 2 jouera le PNJ 3
+}
+
+# Création du modèle
+model = cp_model.CpModel()
+
+# Création des variables pour le modèle
+interventions = {}
+for evt_id, evt in enumerate(evenements):
+    for pnj in evt["pnjs"]:
+        for p in range(nb_personnes):
+            variable_name = f"evt_{evt_id}_pnj_{pnj}_p_{p}"
+            interventions[variable_name] = model.NewBoolVar(variable_name)
+
+# Ajout des contraintes au modèle
+
+# Chaque PNJ doit être joué par une personne à la fois
+for evt_id, evt in enumerate(evenements):
+    for pnj in evt["pnjs"]:
+        model.Add(sum(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] for p in range(nb_personnes)) == 1)
+
+# Les personnes ne peuvent pas jouer plusieurs PNJ en même temps
+for p in range(nb_personnes):
+    for t in range(max(evt["end"] for evt in evenements)):
+        variables_a_temps_t = []
+        for evt_id, evt in enumerate(evenements):
+            for pnj in evt["pnjs"]:
+                if evt["start"] <= t < evt["end"]:
+                    variables_a_temps_t.append(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"])
+        if variables_a_temps_t:
+            model.Add(sum(variables_a_temps_t) <= 1)
+
+# Un PNJ doit être joué par la même personne tout au long du jeu
+for pnj in range(nb_pnj):
+    for p1 in range(nb_personnes):
+        for p2 in range(p1 + 1, nb_personnes):
+            for evt_id1, evt1 in enumerate(evenements):
+                for evt_id2, evt2 in enumerate(evenements):
+                    if pnj in evt1["pnjs"] and pnj in evt2["pnjs"]:
+                        model.Add(interventions[f"evt_{evt_id1}_pnj_{pnj}_p_{p1}"] + interventions[
+                            f"evt_{evt_id2}_pnj_{pnj}_p_{p2}"] <= 1)
+
+# Affectations prédéfinies
+for pnj, personne in affectations_predefinies.items():
+    for evt_id, evt in enumerate(evenements):
+        if pnj in evt["pnjs"]:
+            model.Add(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{personne}"] == 1)
+
+# Définition de l'objectif du modèle
+# ancien objectif
+# temps_total_par_personne = [model.NewIntVar(0, 100, f"temps_total_p_{p}") for p in range(nb_personnes)]
+# for p in range(nb_personnes):
+#     model.Add(temps_total_par_personne[p] == sum(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] * (evt["end"] - evt["start"]) for evt_id, evt in enumerate(evenements) for pnj in evt["pnjs"]))
+# model.Minimize(sum(temps_total_par_personne))
+
+temps_total_par_personne = [model.NewIntVar(0, 100, f"temps_total_p_{p}") for p in range(nb_personnes)]
+for p in range(nb_personnes):
+    model.Add(temps_total_par_personne[p] == sum(
+        interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] * (evt["end"] - evt["start"]) for evt_id, evt in
+        enumerate(evenements) for pnj in evt["pnjs"]))
+
+plus_petit_temps = model.NewIntVar(0, 100, "plus_petit_temps")
+for p in range(nb_personnes):
+    model.Add(plus_petit_temps <= temps_total_par_personne[p])
+model.Maximize(plus_petit_temps)
+
+# Résoudre le modèle
+solver = cp_model.CpSolver()
+status = solver.Solve(model)
+
+# état de la solution
+print(f"solution = {status}")
+
+#####################################################################################################################
+
+
+# Afficher les résultats
+if status in [cp_model.FEASIBLE, cp_model.OPTIMAL]:
+    # if status == cp_model.OPTIMAL:
+    print("Temps total par personne :")
+    for p in range(nb_personnes):
+        print(f"Personne {p}: {solver.Value(temps_total_par_personne[p])} minutes")
+
+    print("\nPlanning des interventions :")
+    for evt_id, evt in enumerate(evenements):
+        for pnj in evt["pnjs"]:
+            for p in range(nb_personnes):
+                if solver.Value(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"]) == 1:
+                    print(f"Personne {p} joue le PNJ {pnj} lors de l'événement {evt_id}")
+                else:
+                    print("Pas de solution trouvée.")
+
+# Trouver la dernière heure de fin des événements
+derniere_heure = max(evt["end"] for evt in evenements)
+
+# Afficher l'en-tête du planning
+print("Heure", end=";")
+for p in range(nb_personnes):
+    print(f"Personne {p}", end=";")
+print()
+
+# Afficher les lignes du planning pour chaque heure
+for t in range(derniere_heure):
+    print(t, end=";")
+    for p in range(nb_personnes):
+        pnj_et_evt = None
+        for evt_id, evt in enumerate(evenements):
+            for pnj in evt["pnjs"]:
+                if evt["start"] <= t < evt["end"] and solver.Value(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"]) == 1:
+                    pnj_et_evt = f"PNJ {pnj} (Evt {evt_id})"
+                    break
+            if pnj_et_evt:
+                break
+
+        if pnj_et_evt:
+            print(f"\t{pnj_et_evt}", end=";")
+        else:
+            print("-", end=";")
+    print()
