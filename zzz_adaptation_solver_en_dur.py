4d434cd73dfeadef09770f95dea7f96e239c7a78
pi-cr <pi-cr@ssji.net>
2023-06-06 01:23:41 +0200

    solveur en dur en cours d'évolution

--- zzz_adaptation_solver_en_dur.py
+++ zzz_adaptation_solver_en_dur.py
@@ -24,13 +24,6 @@ nb_personnes = 4
 #     {"start": 8, "end": 13, "pnjs": [1, 3, 2]},
 # ]
 
-# evenements = [
-#     {'start': 15, 'end': 16, 'pnjs': [2], 'nom': 'E027 - Fête des sports - 1'},
-#     {'start': 12, 'end': 13, 'pnjs': [3], 'nom': 'E004-1 - coup de fil de mr wang - 1'},
-#     {'start': 15, 'end': 16, 'pnjs': [5], 'nom': 'E029 - Le conseil des élèves - 1'},
-#     {'start': 0, 'end': 1, 'pnjs': [4, 6], 'nom': 'E015-1- Il faut sauver Charlie - 1'},
-#     {'start': 8, 'end': 9, 'pnjs': [1, 7], 'nom': 'E008-1 - appel du conseil des observateurs - 1'}]
-
 evenements = [
     {'start': 15, 'end': 16, 'pnjs': ['b'], 'nom': 'E027 - Fête des sports - 1'},
     {'start': 12, 'end': 13, 'pnjs': ['c'], 'nom': 'E004-1 - coup de fil de mr wang - 1'},
@@ -39,18 +32,15 @@ evenements = [
     {'start': 8, 'end': 9, 'pnjs': ['a', 'g'], 'nom': 'E008-1 - appel du conseil des observateurs - 1'}]
 
 # evenements = [
-#     {'start': 15, 'end': 16, 'pnjs': [2]},
-#     {'start': 12, 'end': 13, 'pnjs': [3]},
-#     {'start': 15, 'end': 16, 'pnjs': [5]},
-#     {'start': 0, 'end': 1, 'pnjs': [4, 6]},
-#     {'start': 8, 'end': 9, 'pnjs': [1, 7]}]
+#     {'start': 15, 'end': 16, 'pnjs': ['b'], 'nom': 'E027_-_Fête_des_sports_-_1'}, {'start': 12, 'end': 13, 'pnjs': ['c'], 'nom': 'E004-1_-_coup_de_fil_de_mr_wang_-_1'}, {'start': 15, 'end': 16, 'pnjs': ['e'], 'nom': 'E029_-_Le_conseil_des_élèves_-_1'}, {'start': 0, 'end': 1, 'pnjs': ['d', 'f'], 'nom': 'E015-1-_Il_faut_sauver_Charlie_-_1'}, {'start': 8, 'end': 9, 'pnjs': ['a', 'g'], 'nom': 'E008-1_-_appel_du_conseil_des_observateurs_-_1'}]
+
 
 
 
 
 affectations_predefinies = {
-    1: 0,  # La personne 0 jouera le PNJ 1
-    3: 2,  # La personne 2 jouera le PNJ 3
+    # 1: 0,  # La personne 0 jouera le PNJ 1
+    # 3: 2,  # La personne 2 jouera le PNJ 3
 }
 
 # Création du modèle
@@ -58,16 +48,19 @@ model = cp_model.CpModel()
 
 # Création des variables pour le modèle
 interventions = {}
-for evt_id, evt in enumerate(evenements):
+for evt in evenements:
+    evt_id = evt['nom']
     for pnj in evt["pnjs"]:
         for p in range(nb_personnes):
             variable_name = f"evt_{evt_id}_pnj_{pnj}_p_{p}"
             interventions[variable_name] = model.NewBoolVar(variable_name)
 
+print(f"debug : {interventions}")
 # Ajout des contraintes au modèle
 
 # Chaque PNJ doit être joué par une personne à la fois
-for evt_id, evt in enumerate(evenements):
+for evt in evenements:
+    evt_id = evt['nom']
     for pnj in evt["pnjs"]:
         model.Add(sum(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] for p in range(nb_personnes)) == 1)
 
@@ -75,7 +68,8 @@ for evt_id, evt in enumerate(evenements):
 for p in range(nb_personnes):
     for t in range(max(evt["end"] for evt in evenements)):
         variables_a_temps_t = []
-        for evt_id, evt in enumerate(evenements):
+        for evt in evenements:
+            evt_id = evt['nom']
             for pnj in evt["pnjs"]:
                 if evt["start"] <= t < evt["end"]:
                     variables_a_temps_t.append(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"])
@@ -86,15 +80,18 @@ for p in range(nb_personnes):
 for pnj in range(nb_pnj):
     for p1 in range(nb_personnes):
         for p2 in range(p1 + 1, nb_personnes):
-            for evt_id1, evt1 in enumerate(evenements):
-                for evt_id2, evt2 in enumerate(evenements):
+            for evt1 in evenements:
+                evt_id1 = evt['nom']
+                for evt2 in evenements:
+                    evt_id2 = evt['nom']
                     if pnj in evt1["pnjs"] and pnj in evt2["pnjs"]:
                         model.Add(interventions[f"evt_{evt_id1}_pnj_{pnj}_p_{p1}"] + interventions[
                             f"evt_{evt_id2}_pnj_{pnj}_p_{p2}"] <= 1)
 
 # Affectations prédéfinies
 for pnj, personne in affectations_predefinies.items():
-    for evt_id, evt in enumerate(evenements):
+    for evt in evenements:
+        evt_id = evt['nom']
         if pnj in evt["pnjs"]:
             model.Add(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{personne}"] == 1)
 
@@ -108,8 +105,14 @@ for pnj, personne in affectations_predefinies.items():
 temps_total_par_personne = [model.NewIntVar(0, 100, f"temps_total_p_{p}") for p in range(nb_personnes)]
 for p in range(nb_personnes):
     model.Add(temps_total_par_personne[p] == sum(
-        interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] * (evt["end"] - evt["start"]) for evt_id, evt in
-        enumerate(evenements) for pnj in evt["pnjs"]))
+        interventions[f"evt_{evt['nom']}_pnj_{pnj}_p_{p}"] * (evt["end"] - evt["start"])
+            for evt in evenements for pnj in evt["pnjs"]))
+
+# temps_total_par_personne = [model.NewIntVar(0, 100, f"temps_total_p_{p}") for p in range(nb_personnes)]
+# for p in range(nb_personnes):
+#     model.Add(temps_total_par_personne[p] == sum(
+#         interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"] * (evt["end"] - evt["start"]) for evt_id, evt in
+#         enumerate(evenements) for pnj in evt["pnjs"]))
 
 plus_petit_temps = model.NewIntVar(0, 100, "plus_petit_temps")
 for p in range(nb_personnes):
@@ -134,7 +137,8 @@ if status in [cp_model.FEASIBLE, cp_model.OPTIMAL]:
         print(f"Personne {p}: {solver.Value(temps_total_par_personne[p])} minutes")
 
     print("\nPlanning des interventions :")
-    for evt_id, evt in enumerate(evenements):
+    for evt in evenements:
+        evt_id = evt['nom']
         for pnj in evt["pnjs"]:
             for p in range(nb_personnes):
                 if solver.Value(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"]) == 1:
@@ -156,7 +160,8 @@ for t in range(derniere_heure):
     print(t, end=";")
     for p in range(nb_personnes):
         pnj_et_evt = None
-        for evt_id, evt in enumerate(evenements):
+        for evt in evenements:
+            evt_id = evt['nom']
             for pnj in evt["pnjs"]:
                 if evt["start"] <= t < evt["end"] and solver.Value(interventions[f"evt_{evt_id}_pnj_{pnj}_p_{p}"]) == 1:
                     pnj_et_evt = f"PNJ {pnj} (Evt {evt_id})"
