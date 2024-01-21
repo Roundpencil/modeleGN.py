e8e07a83cb20274e7d2c7e39d8897b7d151da868
pi-cr <pi-cr@ssji.net>
2024-01-21 23:49:48 +0100

    correction d'un bug lié à une spécificité sur la dernière scène des intrigues qui laissaient les balises

--- debugging.py
+++ debugging.py
@@ -2,12 +2,18 @@ from MAGnet import *
 def kalitt_debug():
     gn: GN = GN.load('archive chalacta.mgn')
     noms = [perso.nom for perso in gn.personnages.values()]
-    print(noms)
-    kalitt = next(perso for perso in gn.personnages.values() if 'Kalit' in perso.nom)
+    # print(noms)
+    kalitt = next(perso for perso in gn.personnages.values() if 'Brance' in perso.nom)
     texte_kalitt = generer_squelette_perso(gn, kalitt)
-    print(texte_kalitt)
-    drive, doc, sheet = lecteurGoogle.creer_lecteurs_google_apis()
-
+    # print(texte_kalitt)
+    api_drive, api_doc, sheet = lecteurGoogle.creer_lecteurs_google_apis()
+    nom_fichier = 'test export requete'
+    texte = generer_squelette_perso(gn, kalitt)
+    parent = '1gYWJepb9U2uYOS-4bW5_uLGnFrj5nzmn'
+    mon_id = g_io.creer_google_doc(api_drive, nom_fichier, parent)
+    g_io.write_to_doc(
+        api_doc, mon_id, texte, verbal=True
+    )
 
 
 kalitt_debug()
