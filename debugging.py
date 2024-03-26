d2ef27b2e9c4687096620727103d7e7e999f0ffb
work <pi-work@ssji.net>
2024-03-26 17:05:19 +0100

    travail sur inserteur d'image > rédaction function de texte en cours

--- debugging.py
+++ debugging.py
@@ -75,6 +75,8 @@ def remove_permission_for_email_recursive(api_drive, folder_id, email_address):
                 break
     except HttpError as error:
         print(f'An error occurred: {error}')
+
+
 #     """
 #     Recursively remove permissions for a specific email address from all files and files within subfolders
 #     in a given folder.
@@ -138,14 +140,127 @@ def extraire_tous_items_dossier(api_drive, folder_id):
 #
 ######### RETIRER LES DROITS
 # Initialize your Google Drive API service
-api_drive, _, _ = creer_lecteurs_google_apis()
-root_folder_id = '1_oKXTTD7BtKSI_EWqTjUJ-2jrT_W_e8j'  # le fichier archive
-# root_folder_id = '1CjJPn5Srbka1gqMNt7oAG7pmX-qvgGKN'  # fichier simple de test
-email_address = 'emeric.montagnese@gmail.com'
-remove_permission_for_email_recursive(api_drive, root_folder_id, email_address)
+# api_drive, _, _ = creer_lecteurs_google_apis()
+# root_folder_id = '1_oKXTTD7BtKSI_EWqTjUJ-2jrT_W_e8j'  # le fichier archive
+# # root_folder_id = '1CjJPn5Srbka1gqMNt7oAG7pmX-qvgGKN'  # fichier simple de test
+# email_address = 'emeric.montagnese@gmail.com'
+# remove_permission_for_email_recursive(api_drive, root_folder_id, email_address)
 
 
 # ### COMPTER LES FICHIERS
 # api_drive, _, _ = creer_lecteurs_google_apis()
 # root_folder_id = '1_oKXTTD7BtKSI_EWqTjUJ-2jrT_W_e8j'  # le fichier archive
 # compter_contenu_dossier_drive(root_folder_id, api_drive)
+
+
+######################### travail sur l'insertion d'images automatiques ###############################################
+
+# from googleapiclient.discovery import build
+# from google.oauth2 import service_account
+
+# # Chemin vers votre fichier de clés d'authentification service account JSON
+# SERVICE_ACCOUNT_FILE = 'path/to/service.json'
+#
+# # ID du Google Drive contenant l'image et du Google Doc à modifier
+# GOOGLE_DRIVE_IMAGE_ID = 'votre_id_image'
+# GOOGLE_DOC_ID = 'votre_id_document_google_doc'
+#
+# SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/documents']
+
+
+def inserer_image_dans_google_doc(image_id, doc_id, position, drive_service, docs_service):
+    # Obtenir l'URL de l'image depuis Google Drive
+    image_file = drive_service.files().get(fileId=image_id, fields='webViewLink').execute()
+    image_url = image_file.get('webViewLink')
+
+    # Créer une requête pour insérer l'image dans le Google Doc
+    requests = [
+        {
+            'insertInlineImage': {
+                'location': {
+                    'index': position,
+                },
+                'uri': image_url,
+                'objectSize': {
+                    'height': {
+                        'magnitude': 50,
+                        'unit': 'PT'
+                    },
+                    'width': {
+                        'magnitude': 50,
+                        'unit': 'PT'
+                    }
+                }
+            }
+        }
+    ]
+
+    # # Exécuter la requête
+    # result = docs_service.documents().batchUpdate(documentId=doc_id, body={'requests': requests}).execute()
+    # print(f'Image insérée avec succès : {result}')
+    return requests
+
+
+def lister_images_dans_dossier(folder_id, drive_service):
+    images_dict = {}
+
+    # Définir la requête pour rechercher des fichiers d'images dans le dossier spécifié
+    query = f"'{folder_id}' in parents and (mimeType='image/jpeg' or mimeType='image/png') and trashed = false"
+    response = drive_service.files().list(q=query,
+                                          spaces='drive',
+                                          fields='files(id, name)',
+                                          orderBy='createdTime').execute()
+
+    # Extraire le nom de fichier sans extension et l'ID, et les ajouter au dictionnaire
+    for file in response.get('files', []):
+        # Supprimer l'extension du fichier pour obtenir le nom de l'image
+        file_name_without_extension = '.'.join(file.get('name').split('.')[:-1]).strip()
+        images_dict[file_name_without_extension] = file.get('id')
+
+    return images_dict
+
+
+def prendre_en_compte_prenoms_dans_dico_images(dictionnaire_images):
+    # Copie du dictionnaire pour itérer sur l'original tout en modifiant la copie
+    dictionnaire_modifie = dictionnaire_images.copy()
+
+    for nom_image, id_image in dictionnaire_images.items():
+        # Vérifier si le nom de l'image contient des espaces (donc au moins deux mots)
+        if ' ' in nom_image:
+            # Extraire les caractères avant le premier espace
+            premier_mot = nom_image.split(' ')[0]
+            # Ajouter au dictionnaire une nouvelle clé avec la même valeur
+            dictionnaire_modifie[premier_mot] = id_image
+
+    return dictionnaire_modifie
+
+
+def trouver_clefs_plus_longues_et_positions(dictionnaire, texte):
+    # Dictionnaire pour stocker les positions et les clés correspondantes
+    positions_et_clefs = {}
+
+    # Parcourir chaque clé du dictionnaire
+    for cle in dictionnaire:
+        # Chercher la première apparition de la clé dans le texte
+        position = texte.find(cle)
+        # Si la clé est trouvée dans le texte (position != -1)
+        if position != -1:
+            # Si une clé correspond déjà à cette position, comparer leurs longueurs
+            if position in positions_et_clefs:
+                cle_existante = positions_et_clefs[position]
+                # Conserver la clé la plus longue
+                if len(cle) > len(cle_existante):
+                    positions_et_clefs[position] = cle
+            else:
+                # Ajouter la clé et sa position si aucune clé n'est encore associée à cette position
+                positions_et_clefs[position] = cle
+
+    # Transformer le dictionnaire en liste de listes comme spécifié
+    resultats = [[cle, pos] for pos, cle in positions_et_clefs.items()]
+    return resultats
+
+def test_inclusion_images():
+    api_drive, api_doc, _ = creer_lecteurs_google_apis()
+    # donner un dossier source image, un fichier texte
+
+    # lister toutes les photos des
