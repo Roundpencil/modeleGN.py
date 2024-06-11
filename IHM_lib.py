af6e8b2b18538e4e129784a864d888c8729b8dde
work <pi-work@ssji.net>
2024-06-11 17:24:06 +0200

    maj consmétique

--- IHM_lib.py
+++ IHM_lib.py
@@ -7,9 +7,10 @@ class GidEntry(ttk.Entry):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-    def get(self) -> str:
+    def get(self, verbal=False) -> str:
         raw = super().get()
-        print(f"debug : raw = {raw} / mixed = {g_io.extraire_id_google_si_possible(raw)}")
+        if verbal:
+            print(f"debug : raw = {raw} / mixed = {g_io.extraire_id_google_si_possible(raw)}")
         return g_io.extraire_id_google_si_possible(raw)[0]
   # todo : généraliser l'usage de cette classe chaque fois qu'il y a un champ d'entrée qui prend en compte un id/une url
   #  y compris dans les autres ihms !!
