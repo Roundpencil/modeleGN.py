49e49d8e2bc82e9581bcf8be98f928231db876f7
work <pi-git@ssji.net>
2025-01-16 14:40:51 +0100

    fonctions updates ajoutées, repassage par le nom des classes pour permettre de renomer les classes sans souffrir de la montée de version, à tester

--- updateur_gn.py
+++ updateur_gn.py
@@ -1,67 +1,94 @@
 from packaging import version
-from modeleGN import GN, Personnage, Role, Intrigue, EvenementUnitaire, VERSION
-
-
-# todo :
-#  ajouter des fonctions psécifiques à des versions
+from modeleGN import GN, Personnage, Role, Intrigue, EvenementUnitaire, VERSION, Evenement
 
 # déclaration de la méthode de mise à jour
 def _maj_classe(objet_a_maj, renommages:dict, fonctions_update:dict):
     reference = vars(type(objet_a_maj)())
     current = vars(objet_a_maj)
+    nom_classe = type(objet_a_maj).__name__
+    attributs_a_supprimer = []
+
     # mettre à jour les noms si dans le dictionnaire il y a un nom correspondant
-    if dict_renommage := renommages.get(type(objet_a_maj)):
+    if dict_renommage := renommages.get(nom_classe):
         print(f"debug : dict_renommage :  {dict_renommage}")
         for old_attr, new_attr in dict_renommage.items():
             if hasattr(objet_a_maj, old_attr):
                 valeur_cible = current[old_attr]
                 print(f"debug : l'objet {type(objet_a_maj)} a bien un champ {old_attr} qui vaut {valeur_cible}")
                 setattr(objet_a_maj, new_attr, valeur_cible)
-                delattr(objet_a_maj, old_attr)
+                attributs_a_supprimer.append(old_attr)
+                # delattr(objet_a_maj, old_attr) ancienne version avant le fait de léyaer la suppression
+
     # ajouter les nouveaux champs
     for ref_attr, ref_value in reference.items():
         if not hasattr(objet_a_maj, ref_attr):
             setattr(objet_a_maj, ref_attr, ref_value)
+
+    #appliquer les fonctions
+    if fonction_a_appliquer := fonctions_update.get(nom_classe):
+        fonction_a_appliquer(objet_a_maj)
+
     # supprimer les champs superflus
+    for old_attr in attributs_a_supprimer:
+        delattr(objet_a_maj, old_attr)
+
     old_attrs = list(current.keys())
     for old_attr in old_attrs:
         if old_attr not in reference:
             delattr(objet_a_maj, old_attr)
 
 def _vers_1_4_20240901(gn: GN):
-    renommages = {GN:
+    renommages = {'GN':
                       {'objets': 'objets_de_reference'},
-                  Personnage:
+                  'Personnage':
                       {"orgaReferent": "orga_referent",
                        "joueurs": "interpretes",
                        "sexe": "genre"},
-                  Intrigue:
+                  'Intrigue':
                       {'orgaReferent': 'orga_referent'},
-                  EvenementUnitaire:
+                  'Intervention': #l'ancien nom de evenementunitaire
                       {'heure': 'heure_debut'},
-                  Role:
+                  'Role':
                       {"sexe": "genre"}
                   }
 
-    fonctions_update = {GN:
-                            {},
-                        Personnage:
-                            {},
-                        Intrigue:
-                            {},
-                        EvenementUnitaire:
-                            {},
-                        Role:
-                            {}
+    fonctions_update = {
+                        'Intervention':_intervention_1_4_20240901,# ancien nom de EvenementUnitaire
+                        'Evenement': _evenement_1_4_20240901, #ancien nom de FicheEvenement
                         }
-    version_cible = "1.4.20250113"
+    version_cible = "1.4.20240901"
     # parcours de toutes les classes pour mettre à jour les Objets
 
     _montee_de_version(fonctions_update, gn, renommages, version_cible)
 
+def _intervention_1_4_20240901(evenement_unitaire):
+    if evenement_unitaire.__class__.__name__ == "Intervention":
+        evenement_unitaire.__class__.__name__ = "EvenementUnitaire"
+
+def _evenement_1_4_20240901(evenement):
+    if evenement.__class__.__name__ == "Evenement":
+        evenement.__class__.__name__ = "FicheEvenement"
+
+def _vers_1_2_0(gn: GN):
+    renommages = {
+                  }
+
+    fonctions_update = {Intrigue: _intrigue_1_2_0
+                        }
+    version_cible = "1.2.0"
+    # parcours de toutes les classes pour mettre à jour les Objets
+
+    _montee_de_version(fonctions_update, gn, renommages, version_cible)
+
+def _intrigue_1_2_0(intrigue):
+    intrigue.referent = intrigue.orga_referent
+
 def mettre_a_jour_gn(gn: GN, verbal=True):
 
-    version_fonction = {"1.4.20240901":_vers_1_4_20240901}
+    version_fonction = {
+        "1.2.0": _vers_1_2_0,
+        "1.4.20240901": _vers_1_4_20240901
+    }
     versions = list(version_fonction.keys())
     versions.sort(key=lambda x: version.parse(x))
 
@@ -106,22 +133,10 @@ def _montee_de_version(fonctions_update, gn, renommages, version_cible):
             _maj_classe(scene, renommages, fonctions_update)
             # print(f'heure de la scène {scene.titre} : {scene.heure_debut}')
     for evenement in gn.evenements.values():
-        # todo : déplacer vers les focntions lde la version
-        if evenement.__class__.__name__ == "Evenement":
-            evenement.__class__.__name__ = "FicheEvenement"
-
         _maj_classe(evenement, renommages, fonctions_update)
         for evenement_unitaire in evenement.interventions:
-            # todo : déplacer vers les focntions lde la version
-            if evenement_unitaire.__class__.__name__ == "Intervention":
-                evenement_unitaire.__class__.__name__ = "EvenementUnitaire"
             _maj_classe(evenement_unitaire, renommages, fonctions_update)
     for objet in gn.objets_de_reference.values():
         _maj_classe(objet, renommages, fonctions_update)
-    if version.parse(gn.version) < version.parse('1.2.0'):
-        # todo : déplacer vers les focntions lde la version
-        # dans ce cas il faut mettre à jour les noms des référents car pas automatique
-        intrigues = gn.intrigues.values()
-        for intrigue in intrigues:
-            intrigue.referent = intrigue.orga_referent
+
     gn.version = version_cible
